189bf4d5a2c24b9cf3a4a79992e8ca33b960de79
Johnny JTH <johanohly@gmail.com>
2023-03-01 12:56:03 +0100

    It works

--- src/routes/stem/solve/+page.server.ts
+++ src/routes/stem/solve/+page.server.ts
@@ -1,11 +1,12 @@
 import type { PageServerLoad } from './$types';
+import { WOLFRAM_APP_ID } from '$env/static/private'
 
 export const load = (async ({ url, fetch }) => {
-    if (!process.env.WOLFRAM_APP_ID) throw new Error("WOLFRAM_APP_ID not set");
-    if (!url.searchParams.get("input")) throw new Error("No input provided");
+    if (!url.searchParams.get("input")) return { redirect: "/stem/" };
+    if (!WOLFRAM_APP_ID) return { error: "No Wolfram App ID (This is not an error you can fix)" };
 
-    const response = await fetch(`https://api.wolframalpha.com/v2/query?input=${url.searchParams.get("input")}&format=image,plaintext&output=JSON&appid=${process.env.WOLFRAM_APP_ID}`);
+    const response = await fetch(`https://api.wolframalpha.com/v2/query?input=${url.searchParams.get("input")}&format=image,plaintext&output=JSON&appid=${WOLFRAM_APP_ID}`);
     const result = await response.json();
-    if (!result.queryresult.success) return {error: "Not succesful"};
-    return {};
+    if (!result.queryresult.success) return { error: "Not succesful" };
+    return { result: result.queryresult.pods.map((pod: any) => ({ title: pod.title, steps: pod.subpods.map((subpod: any) => ({text: subpod.plaintext, img: subpod.img.src})) })) };
 }) satisfies PageServerLoad
--- src/routes/stem/solve/+page.svelte
+++ src/routes/stem/solve/+page.svelte
@@ -1,1 +1,24 @@
-It works
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+    onMount(() => {
+        if (data.redirect) goto(data.redirect);
+    });
+</script>
+
+<svelte:head>
+    <title>JohnnyJTH - STEM - Solution</title>
+    <meta name="description" content={`The solution for the question ${data.result[0].steps[0].text}`} />
+</svelte:head>
+
+{#each data.result as result}
+    <div class="w-full border rounded-lg p-4">
+        <h1>{result.title}</h1>
+        {#each result.steps as step}
+            <img src={step.img} alt={step.text} />
+        {/each}
+    </div>
+{/each}
